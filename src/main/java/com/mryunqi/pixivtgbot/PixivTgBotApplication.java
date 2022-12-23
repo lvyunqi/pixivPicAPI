f75720ad1999a2023059615f9a6debb102ca1997
mryunqi <434658198@qq.com>
2022-12-23 23:49:44 +0800

    update

---
+++ src/main/java/com/mryunqi/pixivtgbot/PixivTgBotApplication.java
@@ -0,0 +1,13 @@
+package com.mryunqi.pixivtgbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PixivTgBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PixivTgBotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mryunqi/pixivtgbot/PixivTgBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mryunqi.pixivtgbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PixivTgBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
